8e9319218eee17235e85848c13db0585281ef0a7
Giorgio Garatti <giorgio.garatti@gmail.com>
2022-06-01 15:42:03 +0200

    fix img src

--- modulo-api.js
+++ modulo-api.js
@@ -1,22 +1,3 @@
-/* 
-import {apilist} from "./modulo-api.js";
-import {CONTENT_TYPE} from "./config.js";
-
-console.log("Inizio esecuzione.");
-
-// apilist("harry potter","");
-
-apilist("harry potter", CONTENT_TYPE.MOVIE);
-
-apilist("harry potter","series");
-// filtro i film
-listHarryPotter();
-// filtro le serie televisive
-listSeriesHarryPotter();
-
-console.log("Fine esecuzione...");
-*/
-
 import {BASE_URL} from "./config.js";
 
 // chiama, usando la libreria fetch, le api passando il parametro ricerca e il tipo. Le API restituiscono oggetti json
@@ -31,10 +12,7 @@ export const apilist = (s, type) => {
     });
 }
 
-// estrapola ogni elemento dell'array e stampa le proprietà
 const viewItems = (items) => {
-    // es6 introduce metodi + efficenti, esiste il metodo map, proprio egli array
-    // map cicla l'array e per ogni elemento chiama la funzione di callback con argomento ciascun elemento dell'array
     
     // 1.ciclare l'array
     items.map((item) => {
@@ -42,61 +20,8 @@ const viewItems = (items) => {
         // console.log(item);
         // 3.estrapolare solo le proprietà che ci servono
         //   le prop sono Title, Year, Poster e Type
-        console.group(item.Title);// qui inizia un gruppo di elementi
-        console.log(item.Year);
-        console.log(item.Type);
-        console.log(item.Poster);
-        console.groupEnd();// qui termina il gruppo meglio formattato
+
+        // qui ci sarà il createelement e il createnode
+       
     });
 }
-
-// quando passo una funzione come parametro non metto le parentesi tonde, solo quando la eseguo
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* // usare la mia url e debuggare sul browser
-
-const URL_LIST_HARRY_POTTER = "https://www.omdbapi.com/?apikey=d1b45642&s=harry%20potter";
-
-// chiama la web api con tutti i contenuti di harry potter
-export const listHarryPotter = () => {
-    fetch(URL_LIST_HARRY_POTTER).then((response) => response.json()).then((results) => {
-        const movies = results.Search;
-        console.log(movies);
-    });
-}
-
-// chiama la web api e restituisce solo le serie televisive di "harry potter"
-
-const URL_LIST_SERIES_HARRY_POTTER = "https://www.omdbapi.com/?apikey=d1b45642&s=harry%20potter&type=series";
-
-export const listSeriesHarryPotter = () => {
-    fetch(URL_LIST_SERIES_HARRY_POTTER).then((response) => response.json()).then((results) => {
-        const series = results.Search;
-        console.log(series);
-    });
-} */
-
